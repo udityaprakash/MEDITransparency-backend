73457294c4883257665a4fc501842f668366de16
udityaprakash <udityaprakash983@gmail.com>
2023-11-11 22:33:40 +0530

    file extension .js missed in create

--- SRC/components/hospital/create.js
+++ SRC/components/hospital/create.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const patient = require("../../database/patient");
 
 let create = {
-    medicalrecord:async (req,res)=>{
+    medicalrecord: async (req,res)=>{
         try {
             const id = req.userId;
             const {p_id} = req.body;
@@ -21,9 +21,9 @@ let create = {
                     res.status(404).json({ success:false, msg: 'Invalid patient profile' });
                 }else{
                     var det = {};
-                    Object.assign(jes, {imgurl: "https://meditransparency.onrender.com/hospital/logo/"+det._id});
-                    Object.assign(jes, {name: det.hopital_name});
-                    Object.assign(jes, {hospital_id: det._id});
+                    // Object.assign(jes, {imgurl: "https://meditransparency.onrender.com/hospital/logo/"+det._id});
+                    // Object.assign(jes, {name: det.hopital_name});
+                    // Object.assign(jes, {hospital_id: det._id});
 
                     res.status(404).json({ error: 'Not found' });
                 }
